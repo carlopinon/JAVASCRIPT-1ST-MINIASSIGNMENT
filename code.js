666c3f5ea8d4f941b45a90ffce14d3b3381d5f5f
carlopinon <carlopinon28@gmail.com>
2025-06-19 20:44:00 +0000

    entering code

---
+++ code.js
@@ -0,0 +1,6 @@
+function ageverify(age) 
+{if (age < 18) return "No Minors Allowed"; 
+    else if (age<21) return "You need to be 21+ to drink alcohol"; 
+    else return "Welcome to the Bar!";}
+
+//to this test it out,enter ageverify(age of the person) on the source (browser)
